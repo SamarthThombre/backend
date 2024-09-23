0058aee46ad4c5f08a9e61b3191e97be3e6e65fc
Samarth Thombre <97470376+SamarthThombre@users.noreply.github.com>
2024-09-23 15:28:11 +0000

    add controlers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,66 @@
 import { asyncHandler } from "../utils/asyncHendler.js"
+import { ApiError, APIError } from "../utils/ApiError.js"
+import { User } from "../models/user.model.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
+import { ApiRespponse } from "../utils/ApiResponse.js"
 
 
 const regesterUsers = asyncHandler( async (req, res ) => {
-    res.status(200).json({
-        message: "ok"
+    
+    const { fullName, email, userName, password } = req.body
+    console.log("email: ",email);
+    
+    if (
+        [fullName, email, userName,password].some((field) => 
+        field?.trim() ==="")
+    ) {
+        throw new APIError(400, "All fields are required")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{ userName },{ email }]
     })
+
+    if (existedUser) {
+        throw new APIError(409,"user with email or username already exist")
+    }
+
+    const avataeLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if (!avataeLocalPath) {
+        throw new ApiError( 400," Avatar file not uploded  ")
+    }
+
+    const avatar = await uploadOnCloudinary(avataeLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if (!avatar){
+        throw new ApiError( 400," Avatar file not uploded  ")
+    }
+
+    const user = await User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        userName: userName.toLowerCase()
+    })
+
+   const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+   )
+
+   if (!createdUser ){
+    throw new ApiError(500,"semething went wrong while  regestring the user")
+   }
+
+   return res.status(201).json(
+    new ApiResponce(200, createdUser, "user regesterd sucessfully")
+   )
+
 })
+
 
 export { regesterUsers }
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,8 +1,22 @@
 import { Router } from "express";
 import { regesterUsers } from "../controllers/user.controller.js";
-
+import { upload } from "../middlewares/multer.middleware.js"
 const router = Router()
 
-router.route("/regester").post(regesterUsers)
+router.route("/regester").post(
+    upload.fields(
+        {
+            name:"avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ),
+    
+    regesterUsers
+    )
+
 
 export default router
